1ae79d8fe597b66654a8c464b23962009a6beae9
Dorottya <macidori98@gmail.com>
2021-10-06 22:05:19 +0300

    test alias

--- screens/study/CountryListScreen.js
+++ screens/study/CountryListScreen.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {useSelector} from 'react-redux';
-import {getCountriesOfRegion} from '../../api/Service';
+import {getCountriesOfRegion} from '@api/Service';
 
 const CountryListScreen = ({navigation, route}) => {
   const {region} = route.params;
 
-  //const a = getCountriesOfRegion(region);
+  const a = getCountriesOfRegion(region);
 
-  //const orders = useSelector(state => state.countries);
+  const orders = useSelector(state => state.countries);
 
-  //console.log(orders);
+  console.log(orders);
 
   return (
     <View>

527f9e922eef7b4f183db241ce7420781e1f36ad
Dorottya <macidori98@gmail.com>
2021-10-14 08:42:44 +0300

    dotenv package added, .env created, gitigonre configured

--- babel.config.js
+++ babel.config.js
@@ -12,6 +12,17 @@ module.exports = {
         ],
       },
     ],
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
   ],
   env: {
     production: {
